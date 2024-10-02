9d9a57ce7cbfb7cc67a465fa9ec826af49779d82
신성진 <ssin72787@gmail.com>
2024-10-02 18:02:56 +0900

    [Update] 로그인 작업

--- Assets/3.Script/Etc/FirebaseManager.cs
+++ Assets/3.Script/Etc/FirebaseManager.cs
@@ -1,18 +1,71 @@
+using Firebase;
+using Firebase.Auth;
+using Firebase.Database;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Firebase.Extensions;
+using System.Threading.Tasks;
 
 public class FirebaseManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static FirebaseManager Instance;
+    private FirebaseAuth auth;
+    private DatabaseReference db;
+
+    //[SerializeField] private Text loginLog;
+    //[SerializeField] private Text signUpLog;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     void Start()
     {
-        
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            FirebaseApp app = FirebaseApp.DefaultInstance;
+            auth = FirebaseAuth.DefaultInstance;
+            db = FirebaseDatabase.DefaultInstance.RootReference;
+        });
     }
-
-    // Update is called once per frame
-    void Update()
+    public async Task<bool> Login(string email, string password)
     {
-        
+        try
+        {
+            await auth.SignInWithEmailAndPasswordAsync(email, password);
+            return true;
+        }
+        catch (Firebase.FirebaseException ex)
+        {
+            return false;
+        }
+    }
+    public async Task<bool> SignUp(string email, string password)
+    {
+        try
+        {
+            await auth.CreateUserWithEmailAndPasswordAsync(email, password);
+            return true;
+        }
+        catch (Firebase.FirebaseException ex)
+        {
+            return false;
+        }
+    }
+    public void Logout()
+    {
+        auth.SignOut();
+        SceneManager.LoadScene("Login");
     }
 }
--- Assets/3.Script/Etc/LoginManager.cs
+++ Assets/3.Script/Etc/LoginManager.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class LoginManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Login")]
+    [SerializeField] private GameObject loginPanel;
+    [SerializeField] private InputField loginEmail;
+    [SerializeField] private InputField loginPW;
+    [SerializeField] private Text loginLog;
+    [Header("SignUp")]
+    [SerializeField] private GameObject signUpPanel;
+    [SerializeField] private InputField signUpEmail;
+    [SerializeField] private InputField signUpPW;
+    [SerializeField] private InputField signUpName;
+    [SerializeField] private Text SignUpLog;
+
+    public async void OnLoginBtn()
     {
-        
+        bool success = await FirebaseManager.Instance.Login(loginEmail.text, loginPW.text);
+        if (success)
+        {
+            SceneManager.LoadScene("Lobby");
+        }
+        else
+        {
+            loginLog.text = "error";
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public async void OnSignUpBtn()
     {
-        
+        bool success = await FirebaseManager.Instance.SignUp(signUpEmail.text, signUpPW.text);
+        if (success)
+        {
+            OpenLogin();
+        }
+        else
+        {
+            loginLog.text = "error";
+        }
+    }
+    public void OpenLogin()
+    {
+        signUpPanel.SetActive(false);
+        loginPanel.SetActive(true);
+        loginEmail.text = string.Empty;
+        loginPW.text = string.Empty;
+        loginLog.text = string.Empty;
+    }
+    public void OpenSignUp()
+    {
+        loginPanel.SetActive(false);
+        signUpPanel.SetActive(true);
+        signUpEmail.text = string.Empty;
+        signUpPW.text = string.Empty;
+        signUpName.text = string.Empty;
+        SignUpLog.text = string.Empty;
+    }
+    public void GameExit()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit(); 
+        #endif
     }
 }
